9a6ab169eff4c441bcd368599aef28685dd8fd55
carissalk <81452708+carissalk@users.noreply.github.com>
2024-04-19 18:55:21 -0500

    more ant names hehe

--- Carissa_working.py
+++ Carissa_working.py
@@ -3,7 +3,13 @@
 import random
 
 antNames = ["Antony", "Marie Antoinette", "Tiny Dancer", "Leggy Larry", "Sir March-a-Lot", "Mini Muncher", "Houdini", "Napoleon", 
-            "Six-Stepper","Micro McTiny"]
+            "Six-Stepper","Micro McTiny", "Itsy Bitsy", "Queen of the Hill", "Sherlock Homeslice", "Buzz McKrill", "Colonel Crumb",
+            "Count Crawly", "Pebble Crusher", "Lil' Pincers", "Mighty Mite", "Picasso the Pint-sized", "Scurry McHurry", "Sir Scuttle",
+            "Daisy", "Sparkles", "Bitty", "Cupcake", "Button", "Cinnamon", "Pebbles", "Glimmer", "Sugar", "Tink", "Whiskers", "Velvet",
+            "Skittles", "Twinkle", "Pippin", "Fluffy", "Dottie", "Sprinkles", "Pixel", "Sunny", "Asher", "Beckett", "Caden", "Dexter", 
+            "Emory", "Finn", "Graham", "Holden", "Idris", "Jasper", "Kieran", "Leo", "Milo", "Nolan", "Otto", "Phoenix", "Quinn", "Ronan", 
+            "Silas", "Tobin", "Amelia", "Bella", "Celeste", "Daphne", "Eliza", "Fiona", "Giselle", "Hazel", "Isla", "Juniper", "Kira", 
+            "Luna", "Marigold", "Nova", "Opal", "Penelope", "Quinley", "Rosalyn", "Sadie", "Tessa"]
 
 class Ant:
     def __init__(self, id):
@@ -19,4 +25,3 @@ for ant in range(10):
     ants += [Ant(ant)]
 
 for a in ants: print(a)
-
