e7ef47a4ef85f3bb951a60497ab3ec0a33c24f23
Arianna Brisco <121651388+ariannabrisco@users.noreply.github.com>
2024-05-01 11:54:01 -0500

    more cleanup

--- Tanner/nxConversion.py
+++ Tanner/nxConversion.py
@@ -14,6 +14,7 @@ antNames = ["Antony", "Marie Antoinette", "Tiny Dancer", "Leggy Larry", "Sir Mar
             "Fiona", "Giselle", "Hazel", "Isla", "Juniper", "Kira", "Luna", "Marigold", "Nova", "Opal", "Penelope",
             "Quinley", "Rosalyn", "Sadie", "Tessa", "Arianta", "Tanter", "Carissant"]
 
+
 class Ant:
     def __init__(self, id):
         self.id = id
@@ -37,7 +38,8 @@ class Ant:
         self.tabuList = []
 
     def __repr__(self):
-        return (f"___________\n\n* Ant {self.id} *\nName: {self.name}\nTabu List: {self.tabuList}\n___________\n")
+        return f"___________\n\n* Ant {self.id} *\nName: {self.name}\nTabu List: {self.tabuList}\n___________\n"
+
 
 class AntGraph(nx.Graph):
     def __init__(self, colony=[]):
@@ -47,19 +49,22 @@ class AntGraph(nx.Graph):
 
     def add_node(self, node,  **attr):
         super().add_node(node, **attr)
-        # we may want this to add a random unnocupied ant from the colony later, we may not, for now this function does nothing
-        #self.edges[node][Ant?]
+        # we may want this to add a random unoccupied ant from the colony later, we may not, for now this function does
+        # nothing
+        # self.edges[node][Ant?]
 
     def add_edge(self, u_of_edge, v_of_edge, weight=1,  **attr):
         super().add_edge(u_of_edge, v_of_edge, **attr)
-        # adds a base pheromone intensity to the path, this will later be incremented every time an ant completes his trip across it
+        # adds a base pheromone intensity to the path, this will later be incremented every time an ant completes his
+        # trip across it
         self.edges[u_of_edge, v_of_edge]['weight'] = weight
         self.edges[u_of_edge, v_of_edge]['prob'] = 0
         self.edges[u_of_edge, v_of_edge]['pheromoneIntensity'] = '1'
         
-    # code from aco, it has yet to be converted, some or most of it may have to be completely replaced, I am not yet sure, 
-    # between colony and some of the additional things added by this just being a spicy networkx graph, a lot of it will
-    # need to be reworked and/or can be done easier than it is currently being done, it's all currently pretty messy
+    # code from aco, it has yet to be converted, some or most of it may have to be completely replaced, I am not yet
+    # sure, between colony and some of the additional things added by this just being a spicy networkx graph, a lot of
+    # it will need to be reworked and/or can be done easier than it is currently being done, it's all currently
+    # pretty messy
         
     def calculate_prob(self, edge_to_take, alpha, beta):
         # edge_visibility = 1 / edge_to_take.weight
@@ -76,13 +81,13 @@ class AntGraph(nx.Graph):
         transition_prob = numerator / summation_denominator
         return transition_prob
 
-    def calculate_tour_length(self, tour):  #tour is list of edges
+    def calculate_tour_length(self, tour):  # tour is list of edges
         length = 0
         for town in tour:
             length += self.edges[town]['weight']
         return length
 
-    def optimization(self, Q, alpha, beta, ants):  #ants is a list of ant instance
+    def optimization(self, Q, alpha, beta, ants):  # ants is a list of ant instance
         shortest_path = None
         shortest_path_length = float('inf')
         shuffled_towns = copy.deepcopy(self.nodes)
@@ -97,23 +102,27 @@ class AntGraph(nx.Graph):
         # the rest
         for v in range(len(self.nodes)):
             for ant in ants:
-                curr_vertex = ant.getTown()  #get the current vertex so that we can get its neighbors and respective
-                                             # probalities of going to a neighbor
+                curr_vertex = ant.getTown()  # get the current vertex so that we can get its neighbors and respective
+                # probabilities of going to a neighbor
                 greatest_prob = 0
                 edge_to_take = None
                 vertex_to_go = None
                 potential_edges = []
 
-                for neighbor in curr_vertex.neighbours():  #getting the neighbour town: thi
+                for neighbor in curr_vertex.neighbours():  # getting the neighbour town: thi
                     e = [curr_vertex, neighbor]
                     
-                    if neighbor not in ant.getTabuList():  #if that town has not been visited
+                    if neighbor not in ant.getTabuList():  # if that town has not been visited
                         potential_edges.append(e)
-                    else:  #check this
-                        # e.pher_amt = e.pher_amt + Q / e.weight  #collect trail(pher amt) left by ant on that particular edge
-                        self.edges[e]['pheromoneIntensity'] = self.edges[e]['pheromoneIntensity'] + Q * self.edges[e]['weight']  #collect trail(pher amt) left by ant on that particular edge
-                        # e.prob = self.calculate_prob(e, alpha, beta)  #change the prob of that edge after the ant has left trail (pher amt)
-                        # e.prob = self.calculate_prob(e, alpha, beta)  #change the prob of that edge after the ant has left trail (pher amt)
+                    else:  # check this
+                        # e.pher_amt = e.pher_amt + Q / e.weight  #collect trail(pher amt) left by ant on that
+                        # particular edge
+                        self.edges[e]['pheromoneIntensity'] = self.edges[e]['pheromoneIntensity'] + Q * self.edges[e]['weight']
+                        # collect trail(pher amt) left by ant on that particular edge
+                        # e.prob = self.calculate_prob(e, alpha, beta)  #change the prob of that edge after the ant
+                    #   # has left trail (pher amt)
+                        # e.prob = self.calculate_prob(e, alpha, beta)  #change the prob of that edge after the ant
+                    #   # has left trail (pher amt)
                         self.edges[e]['prob'] = self.calculate_prob(e, alpha, beta)
 
                 for edge in potential_edges:
@@ -127,10 +136,15 @@ class AntGraph(nx.Graph):
 
                 ant.addTown(vertex_to_go)
 
-                # edge_to_take.pher_amt = 0.5 * edge_to_take.pher_amt + Q / edge_to_take.weight  #collect trail(pher amt) left by ant on that particular edge
-                self.edges[edge_to_take]['pheromoneIntensity'] = 0.5 * self.edges[edge_to_take]['pheromoneIntensity'] + Q / self.edges[edge_to_take]['weight']  #collect trail(pher amt) left by ant on that particular edge
-                # edge_to_take.prob = self.calculate_prob(edge_to_take, alpha, beta)  #change the prob of that edge after the ant has left trail (pher amt)
-                self.edges[edge_to_take]['prob'] = self.calculate_prob(edge_to_take, alpha, beta)  #change the prob of that edge after the ant has left trail (pher amt)
+                # edge_to_take.pher_amt = 0.5 * edge_to_take.pher_amt + Q / edge_to_take.weight
+                # collect trail(pher amt) left by ant on that particular edge
+                self.edges[edge_to_take]['pheromoneIntensity'] = (0.5 * self.edges[edge_to_take]['pheromoneIntensity'] +
+                                                                  Q / self.edges[edge_to_take]['weight'])
+                # collect trail(pher amt) left by ant on that particular edge
+                # edge_to_take.prob = self.calculate_prob(edge_to_take, alpha, beta)
+                # change the prob of that edge after the ant has left trail (pher amt)
+                self.edges[edge_to_take]['prob'] = self.calculate_prob(edge_to_take, alpha, beta)  # change the prob of
+                # that edge after the ant has left trail (pher amt)
 
                 # Checking if the current tour is shorter than the globally based shortest path
                 tour_length = self.calculate_tour_length(ant.getTabuList())  # visited vertex is a list vertices
@@ -139,6 +153,8 @@ class AntGraph(nx.Graph):
                     shortest_path_length = tour_length
         
 # G = nx.Graph()
+
+
 G = AntGraph()
 
 nodes = ['a', 'b', 'c', 'd']
@@ -161,4 +177,3 @@ print(type(G.edges['a', 'b']))
     
 nx.draw(G, with_labels=True)
 plt.savefig("filename.png")
-
